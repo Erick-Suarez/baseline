332295a1f88ca41fadd29e6cba5a4ce0a5af521b
mdheri <mantejdheri@gmail.com>
2023-03-26 18:15:26 -0700

    added eslint precommit

--- apps/baseline-chat-backend/index.ts
+++ apps/baseline-chat-backend/index.ts
@@ -1,13 +1,13 @@
-import express, { Express, Request, Response } from "express";
-import * as dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = 300;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server + eslint');
 });
 
 app.listen(port, () => {
